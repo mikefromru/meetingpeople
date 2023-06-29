010efc56f315f120f6169b0ef1ff125de374d631
mikefromru <googlockgood@gmail.com>
2023-06-30 00:48:41 +0300

    Добавил водяной знак к аватаркам

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,6 +6,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 def upload_path(instance, username):
     return f'images/{instance}/{username}'
@@ -20,6 +23,17 @@ class CustomUser(AbstractUser):
     email = models.EmailField(max_length=100, unique=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     avatar = models.ImageField(upload_to=upload_path)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        photo = Image.open(self.avatar.path)
+        watermark = Image.open('app/icon.png') 
+        width, height = photo.size
+        transparent = Image.new('RGBA', (width, height), (0,0,0,0))
+        transparent.paste(photo, (0,0))
+        transparent.paste(watermark, mask=watermark)
+        transparent.show()
+        transparent.save(self.avatar.path)
 
     def __str__(self):
         return self.username
